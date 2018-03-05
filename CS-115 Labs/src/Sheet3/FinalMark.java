2122f3b744de9bbb6633ca67adeef572ac68a778
Chris-Burlacu <thereds9815@yahoo.com>
2018-03-05 17:03:41 +0000

    FirstStage Compleeted

--- CS-115 Labs/src/Sheet3/FinalMark.java
+++ CS-115 Labs/src/Sheet3/FinalMark.java
@@ -16,18 +16,21 @@ public class FinalMark {
 		Scanner in = null;
 		
 		try {
-			in = new Scanner(new File("student_data.txt"));
+			in = new Scanner(new File("student_marks.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println(e);
 		}
 		
-		String student = in.nextLine();
-		Scanner scan = new Scanner(student);
+		String student;
+		Scanner scan;
 		
-		while(scan.hasNext()) {
-			String id = scan.next() + ": ";
-			System.out.println(scan.next() + " " + scan.next() + id + Math.round(((scan.nextInt()*0.1) + (scan.nextInt()*0.1) + (scan.nextInt()*0.8)) *10)/10 );
+		while(in.hasNextLine()) {
+			 student = in.nextLine();
+			 scan = new Scanner(student);
 			
+			String id = "( " + scan.next() + "): ";
+			System.out.println(scan.next() + " " + scan.next() + id + 
+					Math.round(((scan.nextInt()*0.1) + (scan.nextInt()*0.1) + (scan.nextInt()*0.8)) *10.0)/10.0 );			
 			
 		}
 		
